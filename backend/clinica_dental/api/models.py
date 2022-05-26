c77c59e7604901e229fc5d80da1919bdbd1c1a87
Alejandro Flores <alejandro.flores.a@ucb.edu.bo>
2022-05-25 22:59:20 -0400

    Created Medicina, servicio and pagosconsulta

--- backend/clinica_dental/api/models.py
+++ backend/clinica_dental/api/models.py
@@ -9,3 +9,24 @@ class Persona(models.Model):
     fecha_nacimiento = models.DateField()
     def __str__(self):
         return self.id_persona + ' ' + self.nombre
+
+class Servicio(models.Model):
+    id_servicio = models.AutoField(primary_key=True)
+    nombre_servicio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.id_servicio
+
+class PagosConsulta(models.Model):
+    id_pago_consulta =  models.AutoField(primary_key=True)
+    id_consulta = models.ForeignKey(Consulta)
+    monto = models.FloatField()
+
+    def __str__(self):
+        return self.id_pago_consulta
+class Medicina(models.Model):
+    id_med = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255)
+    def __str__(self):
+        return self.id_med

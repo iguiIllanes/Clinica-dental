11139c40dcdc87a20ea280e249d9bbcf21d60459
Ignacio Illanes <igui10082002@gmail.com>
2022-05-27 16:03:37 -0400

    Fix urls

--- backend/clinica_dental/api/urls.py
+++ backend/clinica_dental/api/urls.py
@@ -13,14 +13,7 @@ from .views import (
 router = routers.DefaultRouter()
 
 router = routers.DefaultRouter()
-# router.register(r'personas', views.PersonaViewSet)
-# router.register(r'medicos',views.MedicoViewSet)
-# router.register(r'especialidades',views.EspecialidadViewSet)
-# router.register(r'medicosespecialidades',views.MedicoEspecialidadViewSet)
-# router.register(r'pacientes', views.PacienteViewSet)
-# router.register(r'citas', views.CitaViewSet)
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),

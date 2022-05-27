d77fafb87dec14b355357edbce58269329fe0554
Chris Apaza <chris.apaza@ucb.edu.bo>
2022-05-27 16:02:52 -0400

    Fixed Links Especialidad, MedicoEspecialidad

--- backend/clinica_dental/api/urls.py
+++ backend/clinica_dental/api/urls.py
@@ -3,11 +3,16 @@ from rest_framework import routers
 from . import views
 
 from .views import (
+    MedicosEspecialidadesListApiView,
     PersonasListApiView,
     PersonasDetailApiView,
 
     PacientesListApiView,
     MedicosListApiView,
+    MedicosDetailApiView,
+    EspecialidadesListApiView,
+
+    MedicosEspecialidadesListApiView
 )
 
 router = routers.DefaultRouter()
@@ -27,5 +32,7 @@ urlpatterns = [
     path('personas/', PersonasListApiView.as_view()),
     path('personas/<int:persona_id>/', PersonasDetailApiView.as_view()),
     path('pacientes/', PacientesListApiView.as_view()),
-    path ('medicos/',MedicosListApiView.as_view())
+    path ('medicos/',MedicosListApiView.as_view()),
+    path('medicos/<int:id_medico>/',MedicosDetailApiView.as_view()),
+    path('medicos_especialidades/',MedicosEspecialidadesListApiView.as_view()),
 ]
